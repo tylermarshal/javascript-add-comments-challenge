d8f53f84e083fe97d8813ef314e77ac2543df48f
TMad <madsen.tyler@gmail.com>
2018-03-10 15:37:34 -0700

    add jquery for comment form

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').on('click', (event) => {
+    event.preventDefault();
+    $('#new_comment_form').toggle();
+  })
+  $('#new_comment_form').submit((event) => {
+    event.preventDefault();
+    let author = $('input[name="author"]').val();
+    $('input[name="author"]').val("");
+    let comment = $('textarea[name="comment"]').val();
+    $('textarea[name="comment"]').val("");
+    $("#comment_list").prepend(`<li>${comment} <span class="author">${author}</span></li>`)
+    $('#new_comment_form').toggle();
+  })
 })
